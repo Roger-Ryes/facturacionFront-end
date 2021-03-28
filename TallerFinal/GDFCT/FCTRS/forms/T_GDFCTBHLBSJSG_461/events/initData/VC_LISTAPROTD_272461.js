96844ec43fef46b1da4809bd9a3d3811f949be52
Jean Rave <jean.rave@clearminds-it.com>
2021-03-28 11:40:24 -0500

    Se completaron los eventos cliente del modulo productos

---
+++ TallerFinal/GDFCT/FCTRS/forms/T_GDFCTBHLBSJSG_461/events/initData/VC_LISTAPROTD_272461.js
@@ -0,0 +1,9 @@
+
+     
+//Evento initData : Inicialización de datos del formulario, después de este evento se realiza el seguimiento de cambios en los datos
+//ViewContainer: ListaProductosForm
+task.initData.VC_LISTAPROTD_272461 = function (entities, initDataEventArgs){
+
+    initDataEventArgs.commons.execServer = false;
+
+};
