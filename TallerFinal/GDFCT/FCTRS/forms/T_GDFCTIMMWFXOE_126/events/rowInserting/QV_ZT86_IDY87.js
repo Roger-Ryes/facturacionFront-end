8fc288f283bc50166fec400429cf01c76b7bf6c3
Roger-Rys <roger.reyesm97@gmail.com>
2021-03-28 17:16:19 -0500

    correccion con row query

---
+++ TallerFinal/GDFCT/FCTRS/forms/T_GDFCTIMMWFXOE_126/events/rowInserting/QV_ZT86_IDY87.js
@@ -0,0 +1,13 @@
+
+//gridRowInserting QueryView: QV_ZT86_IDY87
+//Se ejecuta antes de que los datos insertados en una grilla sean comprometidos.
+task.gridRowInserting.QV_ZT86_IDY87 = function (entities,gridRowInsertingEventArgs) {
+
+     gridRowInsertingEventArgs.commons.execServer = true;
+     //gridRowInsertingEventArgs.commons.serverParameters.Cliente = true;
+
+};
+
+
+
+
